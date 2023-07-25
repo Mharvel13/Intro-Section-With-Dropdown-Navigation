5aac88241b12015e943aa927e975d808ca319ec9
Marvel Victor <princevellous@gmail.com>
2023-07-25 06:13:20 +0100

    Finished mobile nav dropdown functionality

--- index.js
+++ index.js
@@ -1,25 +1,55 @@
 // alert('hello')
-
 console.log("hello");
 
+// Features HoverList (Desktop) 
 let features = document.getElementById("features");
 let featuresHoverList = document.getElementById("hover-list-features");
 let featuresIcon = document.getElementById("features-icon");
 
+//  Features DropdownList (Mobile)
+let featuresMobile = document.getElementById("features-mobile");
+let featuresIconMobile = document.getElementById("features-icon-mobile");
+let featuresMobileDropdownList = document.getElementById(
+    "dropdown-list-features"
+);
 
+// let featuresIconMobile = document.getElementById("features-icon");
+
+// Company HoverList (Desktop) 
 let company = document.getElementById("company");
 let companyHoverList = document.getElementById("hover-list-company");
 let companyIcon = document.getElementById("company-icon");
+
+//  Company DropdownList (Mobile)
+let companyMobile = document.getElementById("company-mobile");
+let companyIconMobile = document.getElementById("company-icon-mobile");
+let companyMobileDropdownList = document.getElementById(
+    "dropdown-list-company"
+);
+
+
+
 
 const displayFeaturesDropdown = () => {
     changeImg(featuresIcon);
     featuresHoverList.classList.toggle("none");
 };
 
+const displayFeaturesDropdownMobile = () => {
+    changeImg(featuresIconMobile);
+    featuresMobileDropdownList.classList.toggle("none");
+};
+
 const displayCompanyDropdown = () => {
     changeImg(companyIcon);
 
     companyHoverList.classList.toggle("none");
+};
+
+const displayCompanyDropdownMobile = () => {
+    changeImg(companyIconMobile);
+
+    companyMobileDropdownList.classList.toggle("none");
 };
 
 function changeImg(linkImg) {
@@ -31,34 +61,26 @@ function changeImg(linkImg) {
 }
 
 features.addEventListener("click", displayFeaturesDropdown);
+featuresMobile.addEventListener("click", displayFeaturesDropdownMobile);
+
 company.addEventListener("click", displayCompanyDropdown);
-
-
-
-
-
-
+companyMobile.addEventListener("click", displayCompanyDropdownMobile);
 
 let openBtn = document.getElementById("open-btn");
 let closeBtn = document.getElementById("close-btn");
 let sideBar = document.getElementById("sidebar");
-let mainPage = document.getElementById("main-page");
-
 
 openBtn.addEventListener("click", openSideBar);
+
 closeBtn.addEventListener("click", closeSideBar);
 
 function openSideBar() {
     sideBar.classList.toggle("none");
-    openBtn.style.display = "none";
 }
+
+// let screenWidth = window.screen.width;
+// console.log(screenWidth);
 
 function closeSideBar() {
     sideBar.classList.toggle("none");
-
-    if (window.innerWidth <= 800) {
-        openBtn.style.display = "block";
-    } else {
-        openBtn.style.display = "none";
-    }
 }
